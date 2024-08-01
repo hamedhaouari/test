285de26d11fd4ef2b580d6fe2777485681225c61
Hamed Haouari <hamedalhaouari@gmail.com>
2024-08-01 13:49:10 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -32,7 +32,7 @@ self.addEventListener('push', (event) => {
   const data = event.data.json();
   const options = {
     body: data.message,
-   
+    icon: 'icon.png'
   };
   event.waitUntil(
     self.registration.showNotification(data.title, options)
